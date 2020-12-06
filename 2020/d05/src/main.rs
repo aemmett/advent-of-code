f081ac8ca17a94d257e857c5d57f6778d9fe0e4e
Alec Emmett <>
2020-12-06 16:32:26 -0500

    day 5 part 2

--- 2020/d05/src/main.rs
+++ 2020/d05/src/main.rs
@@ -1,13 +1,29 @@
+use std::convert::TryFrom;
 use std::io::BufRead;
 
 fn main() {
-  let result = std::io::stdin().lock().lines()
+  let mut rows = [0u8; 128];
+
+  std::io::stdin()
+    .lock()
+    .lines()
     .map(|line_res| line_res.unwrap())
     .filter(|line| !line.is_empty())
     .map(|line| decode(&line))
-    .max()
-    .unwrap();
-  println!("{}", result);
+    .for_each(|id| rows[usize::from(id >> 3)] |= 1 << (id & 7));
+
+  let start = rows.iter().position(|&mask| mask > 0).unwrap() + 1;
+  let partial_row_index = start
+    + rows
+      .iter()
+      .skip(start)
+      .position(|&mask| mask < 255)
+      .unwrap();
+  let missing_col = usize::try_from(rows[partial_row_index].trailing_ones()).unwrap();
+  println!("{:?}", rows[partial_row_index]);
+  println!("{:?}", missing_col);
+
+  println!("{}", partial_row_index << 3 | missing_col);
 }
 
 fn decode(bsp: &str) -> u16 {
